5501a2e33b0860627c3ca059aaa9789115089ae3
HolyD2ck <petranatolevic102@gmail.com>
2024-01-25 20:39:28 +0800

    666

--- Data/CourseProjectBlazorContext.cs
+++ Data/CourseProjectBlazorContext.cs
@@ -5,14 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using CourseProjectBlazor.Models;
 
-    public class CourseProjectBlazorContext : DbContext
+public class CourseProjectBlazorContext : DbContext
+{
+    public CourseProjectBlazorContext(DbContextOptions<CourseProjectBlazorContext> options)
+        : base(options)
     {
-        public CourseProjectBlazorContext (DbContextOptions<CourseProjectBlazorContext> options)
-            : base(options)
-        {
-        }
+    }
 
-        public DbSet<CourseProjectBlazor.Models.Employer> Employer { get; set; } = default!;
+    public DbSet<CourseProjectBlazor.Models.Employer> Employer { get; set; } = default!;
 
     public DbSet<CourseProjectBlazor.Models.Applicant> Applicant { get; set; } = default!;
+
+    public void GetData() 
+    {
+        if (Applicant == null) 
+        {
+            var data = ApplicantInfo();
+        }
+    }
+    public List<Applicant> ApplicantInfo()
+    {
+        List<string> lines = File.ReadAllLines("..\\wwwroot\\Base\\applicants.csv").ToList();
+        var Applicant = new List<Applicant>();
+
+
+
+
+        return Applicant;
+    }
 }
